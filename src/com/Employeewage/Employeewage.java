017a6504f127aaf74d69ad22d7abc9c92a262fcc
Robins-2603 <robinsforwork@gmail.com>
2022-06-19 15:53:26 +0530

    Added: UC1_Attendence

--- src/com/Employeewage/Employeewage.java
+++ src/com/Employeewage/Employeewage.java
@@ -3,5 +3,13 @@ package com.Employeewage;
 public class Employeewage {
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage computation program");
+        int Is_Full_time = 1;
+        double empcheck = Math.floor(Math.random()*10) % 2;
+//        System.out.println(empcheck);
+        if (empcheck == Is_Full_time)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Abscent");
+
     }
 }
