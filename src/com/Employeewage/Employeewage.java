1ee19d3e74ad0f752eca8191915e247fbef96675
Robins-2603 <robinsforwork@gmail.com>
2022-06-19 16:22:22 +0530

    Added: Dailywage

--- src/com/Employeewage/Employeewage.java
+++ src/com/Employeewage/Employeewage.java
@@ -4,12 +4,23 @@ public class Employeewage {
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage computation program");
         int Is_Full_time = 1;
+        int Emp_wage_per_hr = 20;
+        int Emp_hrs = 0;
+        int Salary = 0;
         double empcheck = Math.floor(Math.random()*10) % 2;
 //        System.out.println(empcheck);
-        if (empcheck == Is_Full_time)
+        if (empcheck == Is_Full_time) {
             System.out.println("Employee is Present");
-        else
+            Emp_hrs = 8;
+        }
+//        System.out.println();
+        else {
             System.out.println("Employee is Abscent");
+            Emp_hrs = 0;
+        }
+        Salary=Emp_wage_per_hr*Emp_hrs;
+        System.out.println("Daily wage of Employee is "+ Salary);
+
 
     }
 }
