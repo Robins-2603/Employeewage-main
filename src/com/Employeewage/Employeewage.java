4a002e6a5280d91ad642f742916bf99b0aa7fe57
Robins-2603 <robinsforwork@gmail.com>
2022-06-19 15:16:51 +0530

    Added: Employeewage welcome program

---
+++ src/com/Employeewage/Employeewage.java
@@ -0,0 +1,7 @@
+package com.Employeewage;
+
+public class Employeewage {
+    public static void main(String[] args) {
+        System.out.println("welcome to Employee wage computation program");
+    }
+}
