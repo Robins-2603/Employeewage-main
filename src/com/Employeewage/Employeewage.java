3d5d64a2494c53effc3e7807262da8be475bc11f
Robins-2603 <robinsforwork@gmail.com>
2022-06-20 19:10:49 +0530

    Added: Using switch case method

--- src/com/Employeewage/Employeewage.java
+++ src/com/Employeewage/Employeewage.java
@@ -1,30 +1,27 @@
 package com.Employeewage;
 
-import java.util.Random;
-
 public class Employeewage {
+    public static final int Is_Full_time = 1;
+    public static final int Is_part_time = 2;
+    public static final int Emp_wage_per_hr = 20;
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage computation program");
-        int Is_Full_time = 1;
-        int Is_part_time = 2;
-        int Emp_wage_per_hr = 20;
-        int Emp_hrs = 0;
-        int Salary = 0;
-//        Random random=new Random();
+
+        int Emp_hrs;
+        int Salary;
         double empcheck= Math.floor(Math.random()*10) % 3;
-//        System.out.println(empcheck);
-        if (empcheck == Is_Full_time) {
-            System.out.println("Employee is Present");
-            Emp_hrs = 8;
-        }
-//        System.out.println();
-        else if (empcheck == Is_part_time){
-            System.out.println("Employee is Halfday Working");
-            Emp_hrs = 4;
-        }
-        else {
-            System.out.println("Employee is Absent");
-            Emp_hrs = 0;
+        switch ((int) empcheck) {
+            case Is_Full_time:
+                System.out.println("Employee is Present");
+                Emp_hrs = 8;
+                break;
+            case Is_part_time:
+                System.out.println("Employee is Half day Working");
+                Emp_hrs = 4;
+
+            default:
+                System.out.println("Employee is Absent");
+                Emp_hrs = 0;
         }
         Salary=Emp_wage_per_hr*Emp_hrs;
         System.out.println("Daily wage of Employee is "+ Salary);
