f8a8f1766dc9f4f2d10273a2f41eb47e90394e92
Robins-2603 <robinsforwork@gmail.com>
2022-06-20 23:03:28 +0530

    Added: conditional program for wrkhrs 100

--- src/com/Employeewage/Employeewage.java
+++ src/com/Employeewage/Employeewage.java
@@ -5,13 +5,18 @@ public class Employeewage {
     public static final int Is_part_time = 2;
     public static final int Emp_wage_per_hr = 20;
     public static final int no_working_days = 20;
+    public static final int Total_working_hrs = 100;
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage computation program");
 
         int Emp_hrs;
         int Salary;
         int total_salary=0;
-        for (int day=1; day<=no_working_days; day++) {
+        int total_wrk_hrs=0;
+        int total_wrk_days=0;
+
+        while ( total_wrk_hrs<=Total_working_hrs && total_wrk_days<=no_working_days) {
+            total_wrk_days++;
             double empcheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empcheck) {
                 case Is_Full_time:
@@ -29,8 +34,11 @@ public class Employeewage {
             Salary = Emp_wage_per_hr * Emp_hrs;
             System.out.println(Salary);
             total_salary = total_salary + Salary;
+            total_wrk_hrs = total_wrk_hrs + Emp_hrs;
         }
         System.out.println("Total Empwage is " + total_salary);
+        System.out.println("Total EmpHrs is " + total_wrk_hrs);
+        System.out.println("Total EmpDays is " + total_wrk_days);
 
 
 
