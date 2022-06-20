4ab46d0c782f79835549692cba1c6a3aa2c39cf9
Robins-2603 <robinsforwork@gmail.com>
2022-06-20 19:44:53 +0530

    Added: empwage for 20 working days

--- src/com/Employeewage/Employeewage.java
+++ src/com/Employeewage/Employeewage.java
@@ -4,27 +4,34 @@ public class Employeewage {
     public static final int Is_Full_time = 1;
     public static final int Is_part_time = 2;
     public static final int Emp_wage_per_hr = 20;
+    public static final int no_working_days = 20;
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage computation program");
 
         int Emp_hrs;
         int Salary;
-        double empcheck= Math.floor(Math.random()*10) % 3;
-        switch ((int) empcheck) {
-            case Is_Full_time:
-                System.out.println("Employee is Present");
-                Emp_hrs = 8;
-                break;
-            case Is_part_time:
-                System.out.println("Employee is Half day Working");
-                Emp_hrs = 4;
-
-            default:
-                System.out.println("Employee is Absent");
-                Emp_hrs = 0;
+        int total_salary=0;
+        for (int day=1; day<=no_working_days; day++) {
+            double empcheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empcheck) {
+                case Is_Full_time:
+                    System.out.println("Employee is Present");
+                    Emp_hrs = 8;
+                    break;
+                case Is_part_time:
+                    System.out.println("Employee is Half day Working");
+                    Emp_hrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    Emp_hrs = 0;
+            }
+            Salary = Emp_wage_per_hr * Emp_hrs;
+            System.out.println(Salary);
+            total_salary = total_salary + Salary;
         }
-        Salary=Emp_wage_per_hr*Emp_hrs;
-        System.out.println("Daily wage of Employee is "+ Salary);
+        System.out.println("Total Empwage is " + total_salary);
+
 
 
     }
