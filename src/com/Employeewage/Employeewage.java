68496dd8d1247c9489cd01363b2ae48d71515d85
Robins-2603 <robinsforwork@gmail.com>
2022-06-24 03:46:17 +0530

    Added: Empwage computation for multiple companies

--- src/com/Employeewage/Employeewage.java
+++ src/com/Employeewage/Employeewage.java
@@ -6,9 +6,9 @@ public class Employeewage {
     public static final int Is_part_time = 2;
     public static final int Emp_wage_per_hr = 20;
     public static final int no_working_days = 20;
-    public static final int Total_working_hrs = 100;
+    public static final int Max_working_hrs = 100;
     //Defining static method for calculate empwage for company
-    public static int ComputeEmpwage () {
+    public static int ComputeEmpwage (String company, int Emp_wage_per_hr, int no_working_days, int Max_working_hrs) {
 // variables
 
         int Emp_hrs;
@@ -17,7 +17,7 @@ public class Employeewage {
         int total_wrk_hrs=0;
         int total_wrk_days=0;
 
-        while ( total_wrk_hrs<=Total_working_hrs && total_wrk_days<=no_working_days) {
+        while ( total_wrk_hrs<= Max_working_hrs && total_wrk_days<=no_working_days) {
             total_wrk_days++;
             double empcheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empcheck) {
@@ -38,15 +38,16 @@ public class Employeewage {
             total_salary = total_salary + Salary;
             total_wrk_hrs = total_wrk_hrs + Emp_hrs;
         }
-        System.out.println("Total Empwage is " + total_salary);
-        System.out.println("Total EmpHrs is " + total_wrk_hrs);
-        System.out.println("Total EmpDays is " + total_wrk_days);
+        System.out.println("Total Empwage for " +company +" is " + total_salary);
+        System.out.println("Total EmpHrs for " +company +" is " + total_wrk_hrs);
+        System.out.println("Total EmpDays for "+company +" is " + total_wrk_days);
         return total_salary;
 
     }
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage computation program");
-        ComputeEmpwage();
+        ComputeEmpwage("Reliance", 25, 20, 100);
+        ComputeEmpwage("Bridgelab", 20, 24, 150);
 
 
     }
